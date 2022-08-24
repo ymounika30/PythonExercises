e89430db9c410a6ee55fb5272af6e71d02ab20ed
Mounika Yarramasu <my22114@corp.ojas-it.com>
2022-08-24 15:48:17 +0530

    filehandling folder added

---
+++ filehandling/file-handling-ten-exercises-fariha.py
@@ -0,0 +1,48 @@
+'''1.Write a Python program to read last n lines of a file.'''
+##with open("data.txt","r") as f:
+##    r=f.read().split('\n')
+##    print(r[-3:])
+##    
+
+'''2.Write a Python program to read a file line by line and store it into a list.'''
+
+##f=open('data.txt','r')
+##a=f.read().split('\n')
+##print(a)
+##f.close()
+
+'''3.Write a Python program to read a file line by line store it into a variable.'''
+##f=open('data.txt','r')
+##a=f.readlines()
+##s=""
+##for i in a:
+##    s+=i
+##print(s)
+##f.close()
+
+
+'''5. Write a Python program to count the frequency of words in a file'''
+##f=open('data.txt','r')
+##a=f.read()
+##c=0
+##for i in a:
+##    c=c+1
+##print(c)
+##f.close()
+
+'''6. Write a Python program to read a random line from a file.'''
+##import random
+##f=open('data.txt','r')
+##a=random.randint(1,5)
+##r=f.readline()[a]
+##print(r)
+
+
+
+
+
+'''7.Write a Python program to generate 26 text files named A.txt, B.txt, and so on up to Z.txt.
+8.Write a Python program to extract characters from various text files and puts them into a list.
+9.Write a Python program that takes a text file as input and returns the number of words of a given text file
+Note: Some words can be separated by a comma with no space.
+10.Write a Python program to create a file where all letters of English alphabet are listed by specified number of letters on each line'''
